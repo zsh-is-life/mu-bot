b40a800e477047158d5c4c3d002c11a687a0d43d
Abheet <abheet.2011@gmail.com>
2022-05-14 01:46:58 +0530

    add plotting

--- commands/octave.js
+++ commands/octave.js
@@ -1,7 +1,6 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder, SelectMenuOptionBuilder } = require('@discordjs/builders');
 const { exec } = require('child_process');
 const chokidar = require('chokidar');
-
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('octave')
@@ -14,15 +13,21 @@ module.exports = {
 
     async execute(interaction) {
         let input = interaction.options.data[0].value;
-        let regex = [/ls/, /mkdir/, /cd/, /pwd/, /\.\./, /.*\/.*\.(jpg|png)/, /edit/, /movefile/, /.*link/, /rmdir/, /mkfifo/, /umask/, /.stat/, /isdir/, /readdir/, /file_in_path/, /recycle/, /delete/, /glob/, /copyfile/, /rename/];
+        let regex = [/ls/, /mkdir/, /cd/, /pwd/, /\.\./, /print.*\/.*\.(jpg|png)/, /edit/, /movefile/, /.*link/, /rmdir/, /mkfifo/, /umask/, /.stat/, /isdir/, /readdir/, /file_in_path/, /recycle/, /delete/, /glob/, /copyfile/, /rename/, /"/];
         for(let rt of regex) {
             if(rt.exec(input) !== null) {
                 await interaction.reply("Access denied");
+                console.log(rt);
                 console.log(`${interaction.user.tag} was being sneaky`);
                 return;
             }
         }
-        const child = exec(`octave --eval '${input}'`, async function (error, stdout, stderr) {
+
+        if(/plot/.exec(input) !== null) {
+            input = input + '; print -djpg temp.jpg';
+        }
+
+        const child = exec(`octave --eval "${input}"`, async function (error, stdout, stderr) {
             if (error) {
                 console.log(error.stack);
                 console.log('Error code: ' + error.code);
@@ -35,10 +40,15 @@ module.exports = {
                 if(stdout !== "")
                     await interaction.reply(stdout);
                 else
-                    await interaction.reply("Octave sent back no reply...");
+                    await interaction.reply("...");
             }
 
-            }
+        }
         );
+        chokidar.watch('temp.jpg').once('change', async (event, path) => {
+            await new Promise(resolve => setTimeout(resolve, 100));
+            await interaction.channel.send({files: ["./temp.jpg"]});
+        });
+        
     }
 }
